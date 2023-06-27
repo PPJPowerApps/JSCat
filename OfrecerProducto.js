bac68e647bc20615ea8754e1382bbbdaed4e1fc0
Manuel Vidal <imanuel96i@gmail.com>
2023-06-27 16:17:11 -0400

    feat: alert message

--- OfrecerProducto.js
+++ OfrecerProducto.js
@@ -11,9 +11,10 @@ async function OfrecerPowerAutomate(id) {
         idpro: ids[0],
         idcli: ids[1]
     }
+
     try {
         await fetch(
-            "https://prod-00.brazilsouth.logic.azure.com/workflows/20edc70aede1486ebd6dcd31b2a37e53/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ec7VDWnqX9qe6HM4tOA-T1rdXgmi-lWl2JKw6KJOPYA",
+            "https://prod-00.brazilsouth.logic.azure.com:443/workflows/20edc70aede1486ebd6dcd31b2a37e53/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ec7VDWnqX9qe6HM4tOA-T1rdXgmi-lWl2JKw6KJOPYA",
             {
                 method: "POST",
                 headers: {
@@ -22,8 +23,23 @@ async function OfrecerPowerAutomate(id) {
                 body: JSON.stringify(proclie),
             }
         );
-        console.log("Flow call finished Ok");
+        alert('alertmsgsuccess')
     } catch (error) {
+        alert('alertmsgfail')
         console.log(`Flow Call finished with error. Status code: ${error}`);
     }
+}
+
+const alert = (classname) => {
+    const alert = document.getElementById(classname)
+    alert.style.display = "block"
+    alert.style.animation = "fade-in 3s"
+    setTimeout(() => {
+        alert.style.animation = "fade-out 3s"
+        alert.style.display = "block"
+        alert.onanimationend = () => {
+            alert.style.display = "none"
+        }
+    }, 5000)
+    
 }
