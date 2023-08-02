e73feeecdab2b0456df3f57b36fa61472551bf43
Manuel Vidal <imanuel96i@gmail.com>
2023-08-02 17:15:45 -0400

    feat: add new features

--- Recursos/Prospecto/main.js
+++ Recursos/Prospecto/main.js
@@ -1,12 +1,93 @@
-function prospecto(){}
+// const utility = await import('./Utilities/utility.js')
+function prospecto() { }
+
+prospecto.onLoad = (executionContext) => {
+    const formContext = executionContext.getFormContext();
+    // FormType 1 == Create Form
+    if (formContext.ui.getFormType() == 1) {
+        //Ocultar Escala de tiempo y campo ejecutivo en formulario de creacion
+        let data = [
+            formContext.getControl("cr8e5_ejecutivo"),
+            formContext.getControl("Escala")
+        ]
+        hideFields(data);
+    }
+}
 
 prospecto.ValidateRut = (executionContext) => {
+    // const utility = await import('./Utilities/utility.js')
+    // utility.default.test("HOLA")
+    // utility.default.test2("NO FUNCA")
     const formContext = executionContext.getFormContext();
     let rutAttr = formContext.getAttribute("cr8e5_rut");
     let rutCont = formContext.getControl("cr8e5_rut");
     if (rutAttr) {
         validateRut(rutCont, rutAttr)
     }
+}
+
+prospecto.clearForm = (primaryControl) => {
+    const formContext = primaryControl
+    let data = [
+        formContext.getAttribute("cr8e5_name"),
+        formContext.getAttribute("cr8e5_correo"),
+        formContext.getAttribute("cr8e5_rut"),
+        formContext.getAttribute("cr8e5_productoaofrecer")
+    ]
+    clearFields(data)
+}
+
+prospecto.sendBenefits = async () => {
+    const urlPA = "https://prod-01.brazilsouth.logic.azure.com/workflows/a241a98f7a0f4976a3f13b4aae399017/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=1Rh0ZLzd_mrAQilSs65FZp4WUA2qJTlDJrSNacLbtKU"
+    let alert = {
+        sucess: {
+            text: 'El proceso finalizó correctamente',
+            title: 'Correcto',
+            confirmButtonLabel: 'Ok'
+        },
+        error: {
+            text: 'Ocurrió un error, intentelo de nuevo. Si el problema persiste contacte con el administrador',
+            title: 'Error',
+            confirmButtonLabel: 'Aceptar',
+        },
+        options: {
+            height: 200, width: 450
+        },
+    }
+    await callSendBenefits(urlPA, alert)
+}
+
+async function callSendBenefits(url, alert) {
+    try {
+        Xrm.Utility.showProgressIndicator("Please wait...");
+        await fetch(url,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(""),
+            }
+        );
+        Xrm.Utility.closeProgressIndicator();
+        Xrm.Navigation.openAlertDialog(alert.sucess, alert.options);
+    } catch (error){
+        Xrm.Utility.closeProgressIndicator();
+        Xrm.Navigation.openAlertDialog(alert.error, alert.options);
+    }
+}
+
+
+function clearFields(data) {
+    data.map(element => {
+        element ? element.setValue(null) : console.log("Ocurrio un error al limpiar los campos")
+    })
+}
+
+function hideFields(data) {
+    data.map(element => {
+        element ? element.setVisible(false) : console.log("Ocurrio un problema")
+    })
 }
 
 function validateRut(control, rut) {
