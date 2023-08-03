c2ca1ef8961e20a26d78d3e7536bbd4bd69ca27b
Manuel Vidal <imanuel96i@gmail.com>
2023-08-03 17:16:21 -0400

    refactor: erased files

--- LimpiarCampos.js
+++
@@ -1,9 +0,0 @@
-function limpiarCampos(primaryControl) {
-  var formContext = primaryControl;
-  var nombre = formContext.getAttribute("cr8e5_name").setValue(null);
-  var correo = formContext.getAttribute("cr8e5_correo").setValue(null);
-  var rut = formContext.getAttribute("cr8e5_rut").setValue(null);
-  var producto = formContext
-    .getAttribute("cr8e5_productoaofrecer")
-    .setValue(null);
-}
